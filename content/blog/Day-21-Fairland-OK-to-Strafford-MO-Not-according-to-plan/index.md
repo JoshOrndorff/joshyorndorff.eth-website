73673bc62c4c200ed4179bc927b901cb1dbb2203
Joshy Orndorff <admin@joshyorndorff.com>
2026-09-01 16:47:07 -0400

    xc bike trip day 21

---
+++ content/blog/Day-21-Fairland-OK-to-Strafford-MO-Not-according-to-plan/index.md
@@ -0,0 +1,50 @@
++++
+title = "Day 21 -- Fairland OK to Strafford MO -- Not according to plan"
+date = "2013-05-25T04:12:27+00:00"
+tags = ["Bicycle Trip"]
+categories = []
+image = "IMG_20130525_083203.jpg"
++++
+
+Wow, what a crazy day. Just about nothing today went according to plan. 
+
+I woke up feeling pretty discouraged for several reasons. It was cold. I was itchy from all the mosquito bites. The wind was fast and east. My bike had started making a concerning squeaking sound the previous day. And worst of all, my formerly broken arm had started hurting and was a little swollen.
+
+But after a little moping around I managed to get on the road. It was slow going as expected, even slower than yesterday. I was averaging about 19 km/h when I rolled into the first town, Neosho. I planned to get breakfast and some granola bars at the subway/Walmart that I saw on the map, but when I got to the expected location, it was a neighborhood. I ended up wasting about half an hour there before finally getting back to the main road and picking up a sandwich from a gas station.
+
+A little while later I found an ice cream place called Baum's and got a milkshake and some trail mix which made me feel a little better.
+
+After hours of slowly grinding and pedaling down the road the wind finally slowed a tiny bit and shifted from directly in my face to only partially in my face. And a few minutes later I saw a sign saying Springfield (my intended destination) was only 18 miles ahead. Things were starting to look up. I even stopped to take a picture of the sign. I got back on my bike with a slightly improved outlook. But everybody knows what happened next right...
+
+Ping. Broken spoke. Front wheel again. Luckily mom had found me a list of bike shops in Springfield already to get the squeak fixed. Unlucky my phone was dying and my wheel was bent. I pushed the bike back a short ways to the small town of Billings and found an electric outlet on the side of a bar. I looked at the list of shops and saw that one of them was open until 7:00. It was already 5:00 so it would be tight, but I was determined. I managed to roll into the shop at quarter 'til and I bought a new front wheel and rear tire. But I kept a small piece of the old tire to say it made it from ocean to ocean. The mechanic also lubed up my whole drive train and seat post so everything is nice and quiet. He didn't even charge me labor. I have to say I've gotten lucky to find some really great bike shops on this trip.
+
+After leaving the shop I was feeling much better, and my bike hadn't ridden so well since the day I bought it. Quiet and smooth. I was excited to ride it and I had already gone past my intended campground to get to the shop, so I decided to go a bit farther. I had called another campground earlier, but they closed at 8:00. I called again, and she said they could stay open late for me. I told her I would be about an hour, but that turned out to be an underestimate. I called her a few times with updates so she stayed up and waited for me. I rolled in about 9:30. She and her husband were very friendly, and the place even cost less than the planned campground.
+
+I'm excited for a few shorter days coming up and to ride the newly upgraded and tuned bike. I am a bit worried about rain and my arm though. I guess I'll have to feel those out.
+
+Total distance: 191 km
+Average speed: 20.1 km/h
+Trip odometer: 2853 km
+
+Photos:
+
+![Beginning state six](IMG_20130524_092259.jpg)
+![Hey you're getting close, but don't get too excited, it's about to get crazy.](IMG_20130524_163027.jpg)
+![Part of the tire that touched the Pacific](IMG_20130525_083203.jpg)
+![Sun setting over a Missouri field as I rode to the campground](IMG_20130524_202319.jpg)
+
+Comments:
+
+> My finger spanning method is telling me you are now over half way there!!!! Good job, buddy :) (BTW, on a more personal note, I hope you are spreading the word about LUFT and The Zorphans while you are going cross country. This is basically a publicity stunt for both of them, right?) 
+> **tymorehart**
+>
+>> I was just about to ask you for a finger spanning update. It's good to know the halfway point is behind me.
+>> **Josh Orndorff**
+
+> Hey Josh,
+> 
+> Keep it up.   You've made it about 2500KM so far, so that is definitely over halfway, and I'm guessing the hard half is over.  I think you're doing awesome, so don't be too discouraged.   I would refrain from mooning anyone for the rest of the trip unless its an emergency.   You really don't want to put extra strain on that arm.  Maybe find a minute clinic where they can take a quick look at it. 
+> **George Visser**
+>
+>> Thanks for the encouragement man. And good idea about the mooning. You Newberry know when an emergency well crop up though :-) 
+>> **Josh Orndorff**
--- drupal_extraction/BlogConverter.js
+++ drupal_extraction/BlogConverter.js
@@ -63,27 +63,32 @@ uuids = [
 	// "7fd7b9c4-d942-43d8-9bf1-5ab57b1467cd", // NID 1316,  India 1: Mumbai
 
 	// Cross Continent Bike trip
-	"251afce9-974b-416e-bd27-71f84d0d6a0f", // NID 1275, Statistics Suggestions
-	"61009107-f469-4fd1-bd3c-5958e2f25c8a", // Wrapping up thanks
-	"fd17292e-e186-4042-9a9a-19ca06fc445d", // Day 42
-	"98d5ad97-9924-4fb3-919a-bf1762b2c4ec",
-	"7e3dbc56-1eb4-46d0-92ce-c4777b0f00c0",
-	"c6b23ddd-75fd-435b-b429-75823f08702d",
-	"771d5ea8-8454-4032-ae6a-ac71583aa50d",
-	"c5b0b302-13c8-4a9b-8ca8-a58a3d2e8ba1",
-	"9ec31ddc-d9c7-4db0-b3c5-e67c4a6e6c78",
-	"ea2df73d-046d-42c7-ad1c-18607eae05ac",
-	"a247aea7-cd43-484a-bd51-9b59193418ab",
-	"7493f0ad-ff98-4b63-8680-c8d7b8e44482",
-	"1e9907ba-1fb8-44c0-acac-1918f8a80d03",
-	"3b9b9a95-a138-48fd-8a65-99b537e47b3f",
-	"753e3602-e4bb-4fa9-b05f-d63370dfd087",
-	"06c7a522-49ae-4d77-82c3-7dc764c4fd21",
-	"37545a2c-7da8-4df1-8eb1-04f9aab7a106",
-	"0e972538-680f-4245-8020-f6bab96d51dc",
-	"7753f89c-f28c-41e9-953e-92a5d0b8f31c", // Day 26
+	// "251afce9-974b-416e-bd27-71f84d0d6a0f", // NID 1275, Statistics Suggestions
+	// "61009107-f469-4fd1-bd3c-5958e2f25c8a", // Wrapping up thanks
+	// "fd17292e-e186-4042-9a9a-19ca06fc445d", // Day 42
+	// "98d5ad97-9924-4fb3-919a-bf1762b2c4ec",
+	// "7e3dbc56-1eb4-46d0-92ce-c4777b0f00c0",
+	// "c6b23ddd-75fd-435b-b429-75823f08702d",
+	// "771d5ea8-8454-4032-ae6a-ac71583aa50d",
+	// "c5b0b302-13c8-4a9b-8ca8-a58a3d2e8ba1",
+	// "9ec31ddc-d9c7-4db0-b3c5-e67c4a6e6c78",
+	// "ea2df73d-046d-42c7-ad1c-18607eae05ac",
+	// "a247aea7-cd43-484a-bd51-9b59193418ab",
+	// "7493f0ad-ff98-4b63-8680-c8d7b8e44482",
+	// "1e9907ba-1fb8-44c0-acac-1918f8a80d03",
+	// "3b9b9a95-a138-48fd-8a65-99b537e47b3f",
+	// "753e3602-e4bb-4fa9-b05f-d63370dfd087",
+	// "06c7a522-49ae-4d77-82c3-7dc764c4fd21",
+	// "37545a2c-7da8-4df1-8eb1-04f9aab7a106",
+	// "0e972538-680f-4245-8020-f6bab96d51dc",
+	// "7753f89c-f28c-41e9-953e-92a5d0b8f31c", // Day 26
 
-
+	// Begin page 4 in phpmyadmin
+	// "f9f19096-cebf-43b7-ad3b-b95c380176b5", // Day 25
+	// "205eb663-6ad8-4a09-b2cf-dedb0ade5554",
+	// "246ae896-c3c7-4575-baf6-438789bcdca1",
+	// "ea9e47cd-defd-44c0-9ff2-55a581bf4ff7",
+	"f3835128-93e3-4cdd-9e13-c013c287b08f",
 ];
 
 download_all(uuids);
@@ -205,6 +210,16 @@ ${photos.length > 0 ? "Photos:\n" : ""}
 			contents += `![${photo.alt}](${photo.filename})\n`
 		}
 	}
+
+	// Get the comments
+	const commentTree = await getPhotoGalleryComments(uuid);
+
+	// If it is not empty, add them to the contents
+	if (commentTree.length > 0) {
+		const formattedComments = commentsToMarkdown(commentTree);
+		contents += `\nComments:\n\n${formattedComments}`;
+	}
+
 	
 	if (!existsSync(`${dashedTitle}/index.md`)) {
 		writeFileSync(`${dashedTitle}/index.md`, contents, {flag: "w"});
@@ -212,4 +227,140 @@ ${photos.length > 0 ? "Photos:\n" : ""}
 	else {
 		console.log("index file already exists. skipping it");
 	}
+}
+
+
+
+
+
+
+
+
+
+
+// The code below this point was written by duck.ai and is used to extract comments.
+// I only intend to keep comments from bike trip blog posts.
+
+async function getPhotoGalleryComments(nodeUuid) {
+  const params = new URLSearchParams();
+
+  params.set("filter[entity_id.id]", nodeUuid);
+  params.set("include", "uid");
+
+  let nextUrl =
+    `https://joshyorndorff.com/jsonapi/comment/comment_node_photo_gallery?${params}`;
+
+  const rawComments = [];
+  const includedEntities = [];
+
+  while (nextUrl) {
+    const response = await fetch(nextUrl, {
+      headers: {
+        Accept: "application/vnd.api+json"
+      }
+    });
+
+    const responseText = await response.text();
+
+    if (!response.ok) {
+      throw new Error(
+        `Request failed: ${response.status} ${response.statusText}\n` +
+        responseText
+      );
+    }
+
+    const document = JSON.parse(responseText);
+
+    rawComments.push(...(document.data || []));
+    includedEntities.push(...(document.included || []));
+
+    nextUrl = document.links?.next?.href || null;
+  }
+
+  const usersById = new Map();
+
+  for (const entity of includedEntities) {
+    if (entity.type.startsWith("user--")) {
+      usersById.set(entity.id, entity);
+    }
+  }
+
+  const flatComments = rawComments.map(comment => {
+    const attributes = comment.attributes || {};
+    const authorId = comment.relationships?.uid?.data?.id || null;
+    const author = authorId ? usersById.get(authorId) : null;
+
+    const username =
+      attributes.name ||
+      author?.attributes?.name ||
+      null;
+
+    return {
+      id: comment.id,
+      body: attributes.comment_body?.value || "",
+      username,
+      parentId: comment.relationships?.pid?.data?.id || null,
+      replies: []
+    };
+  });
+
+  return formatCommentTree(buildCommentTree(flatComments));
+}
+
+function buildCommentTree(comments) {
+  const commentsById = new Map(
+    comments.map(comment => [comment.id, comment])
+  );
+
+  const topLevelComments = [];
+
+  for (const comment of comments) {
+    if (comment.parentId && commentsById.has(comment.parentId)) {
+      const parent = commentsById.get(comment.parentId);
+      parent.replies.push(comment);
+    } else {
+      topLevelComments.push(comment);
+    }
+  }
+
+  return topLevelComments;
+}
+
+function formatCommentTree(comments) {
+  return comments.map(comment => {
+    const formattedComment = {
+      body: comment.body,
+      username: comment.username
+    };
+
+    if (comment.replies.length > 0) {
+      formattedComment.replies = formatCommentTree(comment.replies);
+    }
+
+    return formattedComment;
+  });
+}
+
+function commentsToMarkdown(comments) {
+  return comments
+    .map(comment => formatComment(comment, 0))
+    .join("\n\n");
+}
+
+function formatComment(comment, depth) {
+  const quotePrefix = ">".repeat(depth + 1);
+  const lines = [];
+
+  for (const bodyLine of comment.body.split(/\r?\n/)) {
+    lines.push(`${quotePrefix} ${bodyLine}`);
+  }
+
+  lines.push(`${quotePrefix} **${comment.username}**`);
+
+  for (const reply of comment.replies || []) {
+    lines.push(`${quotePrefix}`);
+    lines.push(formatComment(reply, depth + 1));
+  }
+
+  return lines.join("\n");
 }
